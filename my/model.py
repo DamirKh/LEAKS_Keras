22ce7acbb62953e74adf496bd1f0678d794591bf
Damir Khakimov <damir.hakimov@gmail.com>
2018-03-14 19:15:00 +0400

    adding some gui (in progress)

--- my/model.py
+++ my/model.py
@@ -10,6 +10,7 @@ from data_reader import ScadaDataFile, VerboseFunc
 
 class LeakTesterModel(object):
     def __init__(self):
+        self.saved = True
         pass
 
     def analyze(self, dataSource,
@@ -46,18 +47,20 @@ class LeakTesterModel(object):
         logging.info("Output classes = %i" % self.NUM_CLASSES)
 
         # normalize and constructing working array
-        data = np.zeros((dataSource.data.shape[0],), dtype=np.float64)
+        data = np.zeros((dataSource.data.shape[0], 1), dtype=np.float64)
         for tag in dataSource.tags_list:
             if tag in self.input_tags:
                 logging.debug("Found tag %s in input data" % tag)
-                data = np.concatenate((data, dataSource.data[..., dataSource.tags_list.index(tag)]), axis=1)
+                tag_data = dataSource.data[..., dataSource.tags_list.index(tag)]
+                data = np.c_[data, tag_data]
                 pass
             else:
                 logging.debug("Skip tag %s in input data" % tag)
         data = np.delete(data, 0, 1)
 
         # normalize all data
-        self.data = 1 / dataSource.data
+        logging.debug('Normalising input data')
+        self.data = 1 / data
 
         self.NUM_OF_FRAMES = self.data.shape[0] - self.TIMESTEPS - 1 - self.VALIDATION_PART
 
@@ -66,30 +69,13 @@ class LeakTesterModel(object):
 
         for frame_ in range(self.NUM_OF_FRAMES):
             # input_string_number =
-            X[frame_] = data[frame_:frame_ + TIMESTEPS, ...]
-            Y[frame_] = data[frame_ + TIMESTEPS + 1]
+            self.X[frame_] = self.data[frame_:frame_ + self.TIMESTEPS, ...]
+            self.Y[frame_] = self.data[frame_ + self.TIMESTEPS + 1]
 
+        logging.info('Input data prepared successfully')
 
-# dataSource = ScadaDataFile()
-# dataSource.load_data('../../data/dataset1')
-# data = 1/dataSource.data
-
-# DATA_DIM = len(dataSource.tags_list)  # ok
-# VALIDATION_PART = int(DATA_DIM/5)     #  ??
-# TIMESTEPS = 100  #
-# BATCH_SIZE = 10
-# NUM_CLASSES = DATA_DIM  # кол-во выходных классов соответствует количеству входных датчиков
-
-# кол-во фрэймов продолжительностью TIMESTEPS во входных данных не включая хвост валидации
-# NUM_OF_FRAMES = data.shape[0]-TIMESTEPS-1-VALIDATION_PART
-
-# X = np.zeros((BATCH_SIZE*NUM_OF_FRAMES, TIMESTEPS, DATA_DIM), dtype=np.float64)
-# Y = np.zeros((BATCH_SIZE*NUM_OF_FRAMES, DATA_DIM), dtype=np.float64)
 
 if False:
-    for frame_ in range(NUM_OF_FRAMES):
-        X[frame_] = data[frame_:frame_ + TIMESTEPS, ...]
-        Y[frame_] = data[frame_ + TIMESTEPS + 1]
 
     X_val = np.zeros((BATCH_SIZE * VALIDATION_PART, TIMESTEPS, DATA_DIM), dtype=np.float64)
     Y_val = np.zeros((BATCH_SIZE * VALIDATION_PART, DATA_DIM), dtype=np.float64)
