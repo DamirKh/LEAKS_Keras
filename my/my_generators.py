a02991e83b0a131f6802044e0db402da52196837
Damir Khakimov <damir.hakimov@gmail.com>
2018-03-16 21:31:16 +0400

    adding generators Полная фигня. запутался. перехожу к созданию stateless LSTM модели

--- my/my_generators.py
+++ my/my_generators.py
@@ -1,8 +1,12 @@
-def generate_(path):
-    while True:
-        with open(path) as f:
-            for line in f:
-                # create Numpy arrays of input data
-                # and labels, from each line in the file
-                x, y = process_line(line)
-                yield (x, y)
+def generate_from_dataSource(data, start_, stop_):
+    """
+
+    :type data: ScadaDataFile
+    """
+    for i in range(start_, stop_):
+        x = data.data[]
+    for data_line in data.data:
+        # create Numpy arrays of input data
+        # and labels, from each line in the file
+        x, y = process_line(line)
+        yield (x, y)
